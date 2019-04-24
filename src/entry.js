a355c8d9a6a9bb3dde79483c18d094023993974d
Scott Thiessen <things@movingobjects.io>
2019-04-24 11:38:52 -0700

    Implement functional thermostat

--- src/entry.js
+++ src/entry.js
@@ -7,7 +7,8 @@ import SAMPLE_DATA_BRAND_DRIVERS from './data/sample_brandDriversGet.json';
 
 let vis1 = {
   chart: d3.select('section#vis-1 svg'),
-  inputRange: d3.select('section#vis-1 .input-range')
+  inputRange: d3.select('section#vis-1 .input-range'),
+  scaleBxpToPerc: d3.scaleLinear().domain([-2000, 2000]).range([0, 1])
 };
 
 (function($) {
@@ -27,20 +28,46 @@ let vis1 = {
     let bxpElem = SAMPLE_DATA_SUMMARY.client.bxpElements.find((elem) => !elem.has_delta),
         bxp     = bxpElem ? bxpElem.score : 0;
 
-    updateBxp(bxp);
-
     vis1.chart
       .attr('width', 500)
       .attr('height', 500);
+
+    vis1.chart
+      .append('rect')
+        .attr('x', 200)
+        .attr('y', 100)
+        .attr('width', 100)
+        .attr('height', 300)
+        .attr('fill-opacity', 0)
+        .attr('stroke-width', 1)
+        .attr('stroke', 'black');
+
+    vis1.chartBar = vis1.chart
+      .append('rect')
+        .attr('x', 202)
+        .attr('y', 102)
+        .attr('width', 96)
+        .attr('height', 296);
 
     vis1.inputRange
       .on('input', function() {
         updateBxp(this.value);
       });
 
+    updateBxp(bxp);
+
   }
 
   function updateBxp(val) {
+
+    let perc = vis1.scaleBxpToPerc(val);
+
+    let barH = 296 * perc,
+        barY = 398 - barH;
+
+    vis1.chartBar
+      .attr('height', barH)
+      .attr('y', barY)
 
     vis1.inputRange
       .property('value', val);
